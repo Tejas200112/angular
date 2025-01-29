097dac3c6e02c7ac2299b70d85b54e7ba3782409
Tejas200112 <tejasmashere2001@gmail.com>
2025-01-29 12:33:44 +0530

    first file

---
+++ amazonproduct.ts
@@ -0,0 +1,10 @@
+type productid=number
+type productname=string
+type productprize=number
+
+let id:productid=1
+let name:productname="shoes"
+let prize:productprize=1000
+
+console.log("amazon producdetails")
+console.log("amazon productid name is"+productname+"product name"+productname+"prize"+prize+)
